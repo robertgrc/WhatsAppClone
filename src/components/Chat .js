e40ebe958575e00f75e20c1621d2dc3866b678af
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-04-01 11:29:15 -0400

    WhatsApp ChatFooter in process

---
+++ src/components/Chat .js
@@ -0,0 +1,52 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  AttachFile,
+  InsertEmoticon,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
+
+import "./Chat.css";
+function Chat() {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3> Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Sonny Sangha</span>Hey Guys
+          <span className="chat__timestamp">3.52pm</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form></form>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAljB9zEzzJdLFn861JsmJ7dlRFIdJRYA",
+  authDomain: "whats-app-clone-44a11.firebaseapp.com",
+  projectId: "whats-app-clone-44a11",
+  storageBucket: "whats-app-clone-44a11.appspot.com",
+  messagingSenderId: "544286728328",
+  appId: "1:544286728328:web:02de8f594f01d7c7f23f3d",
+};
